9a6d158a11810037a56a6a9ee3a26967edf27c9f
Sylar Chen <chenheng415@163.com>
2016-09-03 22:40:19 +0800

    error handle example

---
+++ src/errorhandle.js
@@ -0,0 +1,14 @@
+const express = require('express')  
+const app = express()
+
+app.get('/', (request, response) => {  
+  throw new Error('I always throw the exception')
+})
+
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke: ' + err)
+})
+
+app.listen(3003)
